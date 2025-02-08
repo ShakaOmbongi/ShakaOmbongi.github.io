9a027cf088f9662d2366df2ed6326efb8c07394c
shakaombongi <shakaombongi@gmail.com>
2025-02-08 00:37:28 -0500

    shop now

--- 372OnlineStore/function.js
+++ 372OnlineStore/function.js
@@ -1,12 +1,16 @@
-// making affects when button is pressed
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll(".btn, button");
     
     buttons.forEach(function (btn) {
       btn.addEventListener("click", function () {
-        // pressed class on the clicked button
+        //  pressed class on click for visual feedback
         btn.classList.toggle("pressed");
         console.log("Button clicked:", btn.innerText);
+        
+        // add to cart alert
+        if (btn.innerText.includes("Add to Cart")) {
+          alert("Item added to cart!");
+        }
       });
     });
   });
